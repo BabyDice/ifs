791ce3547eddc739d1c8da9ab098caefb9c292b1
Anthony <anthony.bellier@laposte.net>
2022-06-13 15:10:20 +0200

    navbar en cours

--- src/components/navbar/PageNavbar.vue
+++ src/components/navbar/PageNavbar.vue
@@ -4,28 +4,60 @@
   <nav class="navbar">
     <nav>
       <ul class="main">
-        <li><router-link to="/">Accueil</router-link></li>
-        <li class="formation">
-          <router-link to="/formation">Formation</router-link>
+        <li>
+          <router-link :to="{ name: 'accueil' }">Accueil</router-link>
+        </li>
+        <!-- <li class="DeroulantFormation">
+          <router-link :to="{ name: 'formation' }">Formation</router-link>
           <ul class="sous">
-            <li><router-link to="">Bureautique</router-link></li>
+            <li>
+              <router-link :to="{ name: 'bureau' }">Bureautique</router-link>
+            </li>
+          </ul>
+        </li> -->
+        <li class="deroulant">
+          <router-link :to="{ name: 'formation' }">Formation</router-link>
+          <ul class="sous">
+            <li>
+              <router-link :to="{ name: 'bureau' }">Bureautique</router-link>
+            </li>
           </ul>
         </li>
-        <li><img src="../../assets/logo.png" width="50px" height="50px" /></li>
-        <li><router-link to="/propos">A propos</router-link></li>
-        <li><router-link to="/contact">Contact</router-link></li>
+        <li>
+          <img
+            src="../../assets/navbar/logo_final_ifs78.svg"
+            width="50px"
+            height="50px"
+          />
+        </li>
+        <li>
+          <router-link :to="{ name: 'propos' }">A propos</router-link>
+        </li>
+        <li>
+          <router-link :to="{ name: 'contact' }">Contact</router-link>
+        </li>
       </ul>
     </nav>
   </nav>
 </template>
 
 <style>
+* {
+  font-family: "Poppins";
+  font-style: normal;
+  font-weight: 500;
+  font-size: 24px;
+  line-height: 30px;
+}
+
 nav {
   background: rgba(255, 255, 255, 0.25);
   box-shadow: 0px 4px 15px rgba(0, 0, 0, 0.25);
   backdrop-filter: blur(15px);
   display: flex;
   flex-direction: column;
+  width: 100%;
+  height: 100%;
 }
 
 .main {
@@ -34,14 +66,55 @@ nav {
   list-style: none;
   justify-content: center;
   align-items: center;
+  width: clamp(100%, 40%, 15%);
 }
 
 li {
   flex: 2;
 }
 
-.formation:hover {
+nav a:hover {
+  color: orange;
+  border-bottom: 2px solid gold;
 }
+
+.sous {
+  display: none;
+  box-shadow: 0px 1px 2px #ccc;
+  background-color: white;
+  position: absolute;
+  /* margin-top: 5px; */
+  padding: 4%;
+  width: 20%;
+  z-index: 1000;
+}
+
+nav > ul li:hover .sous {
+  display: block;
+}
+
+.sous li {
+  float: none;
+  width: 0%;
+  text-align: center;
+}
+
+.sous a {
+  padding: 10px;
+  border-bottom: none;
+}
+
+.sous a:hover {
+  border-bottom: none;
+  background-color: RGBa(200, 200, 200, 0.1);
+}
+
+.deroulant > a::after {
+  content: " ▼";
+  font-size: 24px;
+}
+
+@media screen (max-width: 1024px;);
 </style>
 
 <script>
