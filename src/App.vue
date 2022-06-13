bfaa38b2136be27cf2c24fd66302d749495bd4fe
BabyDice <secure.contain.protect10888@gmail.com>
2022-06-13 12:16:03 +0200

    nothing just for overwrite the stash

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <nav>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">Aboute</router-link>
-      <router-link to="/formation">Aboute</router-link>
+      <router-link to="/formation">formation</router-link>
     </nav>
     <router-view />
   </div>
