b77b4fd655c87b35753f224056af1010bf2ce8df
Anthony <anthony.bellier@laposte.net>
2022-06-15 09:53:00 +0200

    master Page & scss default margin

--- src/router/index.js
+++ src/router/index.js
@@ -11,32 +11,32 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () =>
-      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+      import(/* webpackChunkName: "about" */ "@/views/AboutView.vue"),
   },
   {
     path: "/",
     name: "accueil",
-    component: () => import("../views/AccueilView.vue"),
+    component: () => import("@/views/AccueilView.vue"),
   },
   {
     path: "/formation",
     name: "formation",
-    component: () => import("../views/FormationView.vue"),
+    component: () => import("@/views/FormationView.vue"),
   },
   {
     path: "/bureau",
     name: "bureau",
-    component: () => import("../views/BureautiqueView.vue"),
+    component: () => import("@/views/BureautiqueView.vue"),
   },
   {
     path: "/contact",
     name: "contact",
-    component: () => import("../views/ContactView.vue"),
+    component: () => import("@/views/ContactView.vue"),
   },
   {
     path: "/propos",
     name: "propos",
-    component: () => import("../views/ProposView.vue"),
+    component: () => import("@/views/ProposView.vue"),
   },
 ];
 
