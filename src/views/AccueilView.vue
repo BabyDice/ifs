7612873a848b8f37e10b14329a561a94bffe8782
Anthony <anthony.bellier@laposte.net>
2022-06-27 10:33:21 +0200

    contact emailjs

--- src/views/AccueilView.vue
+++ src/views/AccueilView.vue
@@ -1,11 +1,18 @@
 <template>
-  <div>Vous etes sur la page Accueil</div>
+  <div>
+    Vous etes sur la page Accueil
+    <ContactValiduateVue />
+    <EmailJSVue />
+  </div>
 </template>
 
 <style></style>
 
 <script>
+import ContactValiduateVue from "@/composants/ContactValiduate.vue";
+import EmailJSVue from "@/composants/EmailJS.vue";
 export default {
   name: "AccueilView",
+  components: { ContactValiduateVue, EmailJSVue },
 };
 </script>
