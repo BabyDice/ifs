ef194b995263d40592533a4b038915e257d60aac
Anthony <anthony.bellier@laposte.net>
2022-06-30 16:44:11 +0200

    form contact fini

--- src/composants/VuelidateForm.vue
+++ src/composants/VuelidateForm.vue
@@ -2,79 +2,120 @@
   <div class="formulaire">
     <form @submit.prevent="submit" ref="form" class="form">
       <div class="form__group" :class="{ 'form-group--error': $v.name.$error }">
+        <p class="typo__p" v-if="submitStatus === 'ERROR'">
+          Veuillez remplir les champs obligatoires.
+        </p>
         <label class="form__group__label">Name</label>
         <input class="form__group__input" v-model.trim="$v.name.$model" />
+        <div class="error" v-if="!$v.name.required">Votre nom est attendu</div>
+        <div class="error" v-if="!$v.name.minLength">
+          Votre Nom doit contenir {{ $v.name.$params.minLength.min }} lettres
+          minimum.
+        </div>
         <label class="form__group__label">Mail</label>
         <input class="form__group__input" v-model.trim="$v.mail.$model" />
-        <button
-          class="button"
-          type="submit"
-          :disabled="submitStatus === 'PENDING'"
-        >
-          Envoyer
-        </button>
-      </div>
-      <div class="form__group" :class="{ 'form-group--error': $v.name.$error }">
+        <div class="error" v-if="!$v.mail.required">
+          Votre email est attendu
+        </div>
+        <!-- <div class="error" v-if="!$v.mail.minLength">
+          Name must have at least {{ $v.mail.$params.minLength }} Password.
+        </div> -->
         <label class="form__msg__label">Message</label>
         <textarea
           class="form__msg__input"
           v-model.trim="$v.msg.$model"
+          rows="10px"
         ></textarea>
-        <button class="button" type="reset">Annuler</button>
-      </div>
-      <!-- <div class="error" v-if="!$v.name.required">Votre nom est attendu</div>
-      <div class="error" v-if="!$v.name.minLength">
-        Votre Nom doit contenir {{ $v.name.$params.minLength.min }} lettres
-        minimum.
+        <div class="error" v-if="!$v.msg.required">
+          Votre message est attendu
+        </div>
+        <div class="error" v-if="!$v.msg.minLength">
+          Votre Message doit contenir {{ $v.msg.$params.minLength.min }} lettres
+          minimum.
+        </div>
+        <div class="form__btn">
+          <button
+            class="button"
+            type="submit"
+            :disabled="submitStatus === 'PENDING'"
+          >
+            Envoyer
+          </button>
+          <button class="button" type="reset">Annuler</button>
+        </div>
+        <p class="typo__p" v-if="submitStatus === 'OK'">
+          Merci pour votre envoie!
+        </p>
       </div>
 
-      <div class="error" v-if="!$v.mail.required">Votre email est attendu</div>
-      <div class="error" v-if="!$v.mail.minLength">
-        Name must have at least {{ $v.mail.$params.minLength }} Password.
-      </div>
-
-      <div class="error" v-if="!$v.msg.required">Votre message est attendu</div>
-      <div class="error" v-if="!$v.msg.minLength">
-        Votre Message doit contenir {{ $v.msg.$params.minLength.min }} lettres
-        minimum.
-      </div> -->
-
-      <p class="typo__p" v-if="submitStatus === 'OK'">
-        Merci pour votre envoie!
-      </p>
-      <p class="typo__p" v-if="submitStatus === 'ERROR'">
-        Veuillez remplir les champs obligatoires.
-      </p>
-      <p class="typo__p" v-if="submitStatus === 'PENDING'">Sending...</p>
+      <p class="typo__p" v-if="submitStatus === 'PENDING'">Envoie...</p>
     </form>
   </div>
 </template>
 
 <style lang="scss">
+@import "../_variablesscss";
 .formulaire {
   .form {
     border: 1px solid black;
     border-radius: 20px;
     display: flex;
-    gap: 25%;
-    justify-content: center;
-    align-items: center;
-    width: 50%;
-    height: 550px;
+    height: 850px;
+    width: 80%;
+    background: #f8f8f8;
     margin: auto;
 
+    .typo__p {
+      font-size: 16px;
+      font-family: $fontfamilyMedium;
+    }
+
     &__group {
-      background-color: red;
-      flex-direction: column;
       display: flex;
-      height: 100%;
-      width: 100%;
-      justify-content: flex-start;
-      align-items: center;
+      flex-direction: column;
+      margin: auto;
+      width: 80%;
+      gap: 24px;
+      label {
+        display: flex;
+        font-family: $fontfamilyMedium;
+        font-size: 16px;
+      }
+
+      input {
+        line-height: 10vh;
+        padding: 2%;
+        border-radius: 15px;
+        border: none;
+        background: rgba(210, 210, 210, 0.16);
+        border: 1px solid #000000;
+        box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.15);
+      }
+
+      textarea {
+        border: none;
+        border-radius: 15px;
+        padding: 2%;
+        background: rgba(210, 210, 210, 0.16);
+        border: 1px solid #000000;
+        box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.15);
+      }
+    }
+
+    &__btn {
+      display: flex;
+      justify-content: space-around;
       gap: 24px;
 
-      // &__label {
-      // }
+      button {
+        width: 50%;
+        border: none;
+        border-radius: 15px;
+        line-height: 6vh;
+        background: rgba(210, 210, 210, 0.16);
+        border: 1px solid #000000;
+        box-shadow: 0px 4px 10px rgba(0, 0, 0, 0.15);
+      }
     }
   }
 }
