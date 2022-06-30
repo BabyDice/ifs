fd55e9d433ceebc5614bed0277d96565559964c3
Anthony <anthony.bellier@laposte.net>
2022-06-30 16:59:30 +0200

    merge verif

--- src/views/AccueilView.vue
+++ src/views/AccueilView.vue
@@ -1,18 +1,11 @@
 <template>
-  <div>
-    Vous etes sur la page Accueil
-    <ContactValiduateVue />
-    <!-- <EmailJSVue /> -->
-  </div>
+  <div>Vous etes sur la page Accueil</div>
 </template>
 
 <style></style>
 
 <script>
-import ContactValiduateVue from "@/composants/VuelidateForm.vue";
-// import EmailJSVue from "@/composants/EmailJsForm.vue";
 export default {
   name: "AccueilView",
-  components: { ContactValiduateVue },
 };
 </script>
